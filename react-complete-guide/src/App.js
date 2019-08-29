cd6b6bb5e6b425145d1c48c41c6e92d2dc6ba9a1
Kosek Jan <Jan.Kosek@tescosw.cz>
2019-08-29 11:21:51 +0200

    simple component with props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>sup</h1>
+        <Person name="Lulec" age="69"></Person>
+        <Person name="Pulec" age="13"></Person>
       </div>
     );
   }
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = (props) => {
+    return <p>{props.name} here! I am {props.age}</p>
+}
+
+export default person;
