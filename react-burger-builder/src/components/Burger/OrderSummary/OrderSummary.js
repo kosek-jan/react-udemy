b7bcb9a2150c1d6e4f71deb0b88ba1f4c3e6744e
kosek-jan <smashercz1@gmail.com>
2019-10-08 22:50:15 +0200

    burger builder 3

---
+++ react-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+    const ingredientsSummary = Object.keys(props.ingredients).map(igKey => {
+        return <li key={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>
+    });
+    return (
+        <Aux>
+            <p>Your Order</p>
+            <p>A delicious burger consists of:</p>
+            <ul>
+                {ingredientsSummary}
+            </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)},-</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </Aux>
+    )
+};
+
+export default orderSummary;
---
+++ react-burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
+const layout = (props) => (
+    <Aux>
+        <Toolbar />
+        <main className={styles.content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
