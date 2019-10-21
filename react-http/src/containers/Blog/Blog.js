a8540fb7edd55d59ad24417c0ba9602eec706312
kosek-jan <smashercz1@gmail.com>
2019-10-21 20:57:07 +0200

    routing #2

--- react-http/src/containers/Blog/Blog.js
+++ react-http/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -13,17 +13,22 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: '/new-post',
+                            <li><NavLink to="/posts" exact 
+                                // activeClassName="my-active"
+                            >Posts</NavLink></li>
+                            <li><NavLink to={{
+                                // pathname: this.props.match.url + '/new-post', // relativní cesta
+                                pathname: '/new-post', // relativní cesta
                                 hash: '#submit',
                                 search: '?quick-sumbit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                </Switch>
             </div>
         );
     }
--- react-http/src/containers/Blog/FullPost/FullPost.js
+++ react-http/src/containers/Blog/FullPost/FullPost.js
@@ -8,10 +8,26 @@ class FullPost extends Component {
         loadedPost: null
     }
 
+    componentDidMount(){
+        console.log(this.props);
+        this.loadData();
+    }
+    
     componentDidUpdate(){
-        if (this.props.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+        this.loadData();
+    }
+    
+    deleteDataHandler = () => {
+        axios.delete('/posts/' + this.props.match.params.id)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
+    loadData(){
+        if (this.props.match.params.id) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
+                axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         this.setState({loadedPost: response.data});
                     });
@@ -19,17 +35,9 @@ class FullPost extends Component {
         }
     }
 
-    
-    deleteDataHandler = () => {
-        axios.delete('/posts/' + this.props.id)
-            .then(response => {
-                console.log(response);
-            });
-    }
-
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if (this.props.id){
+        if (this.props.match.params.id){
             post = <p style={{textAlign: 'center'}}>Loading...!</p>;
         }
         if (this.state.loadedPost){
