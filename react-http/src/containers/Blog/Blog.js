2f90018c847225be6faacb2e537775cf025f23e3
Kosek Jan <Jan.Kosek@tescosw.cz>
2019-11-05 16:45:44 +0100

    finished routing

--- react-http/src/containers/Blog/Blog.js
+++ react-http/src/containers/Blog/Blog.js
@@ -4,9 +4,17 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+    state = {
+        auth: false,
+    }
     render(){
         return (
             <div className="Blog">
@@ -26,8 +34,11 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/new-post" component={AsyncNewPost} />
+                    {/* {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null} */}
                     <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Ooups, Not found</h1>}/> {/* Catch all/unknown routes 404 */}
+                    {/* <Redirect from="/" to="/posts" /> */}
                 </Switch>
             </div>
         );
--- react-http/src/containers/Blog/NewPost/NewPost.js
+++ react-http/src/containers/Blog/NewPost/NewPost.js
@@ -10,6 +10,10 @@ class NewPost extends Component {
         author: 'smasha'
     }
 
+    componentDidMount() {
+        // if unauth => this.props.history.replace('/posts');
+    }
+
     postDataHandler = () => {
         const post = {
             title: this.state.title,
