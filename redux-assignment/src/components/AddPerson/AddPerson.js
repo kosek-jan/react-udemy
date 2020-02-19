9e50de6f1296d235e5e36e92a2d6f9b8dfd0d60e
Kosek Jan <Jan.Kosek@tescosw.cz>
2020-02-19 15:24:47 +0100

    redux with local state

---
+++ redux-assignment/src/components/AddPerson/AddPerson.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+import './AddPerson.css';
+
+class AddPerson extends Component {
+    state = {
+        name: '',
+        age: ''
+    }
+
+    nameChangedHandler = (e) => {
+        this.setState({name: e.target.value});
+    }
+    ageChangedHandler = (e) => {
+        this.setState({age: e.target.value});
+    }
+    render (){
+        return (
+            <div className="AddPerson">
+                <input type="text" placeholder="Name" onChange={this.nameChangedHandler} value={this.state.name}></input>
+                <input type="number" placeholder="Age" onChange={this.ageChangedHandler} value={this.state.age}></input>
+                <button onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+            </div>
+        )
+    }
+} 
+
+export default AddPerson;
