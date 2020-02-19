daeb93eb3370006a710e2d18080b23a55c9ab2ce
Kosek Jan <Jan.Kosek@tescosw.cz>
2020-02-19 11:59:34 +0100

    form validation

--- react-burger-builder/src/components/UI/Input/Input.js
+++ react-burger-builder/src/components/UI/Input/Input.js
@@ -4,16 +4,30 @@ import styles from './Input.module.css';
 
 const input = (props) => {
     let inputElement = null;    
+    const inputStyles = [styles.InputElement];
 
-    switch (props.inputtype) {
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputStyles.push(styles.Invalid);
+    }
+
+    switch (props.elementType) {
         case ( 'input' ):
-            inputElement = <input className={styles.InputElement} {...props} />;
+            inputElement = <input className={inputStyles.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ( 'textarea' ):
-            inputElement = <textarea className={styles.InputElement} {...props} />;
+            inputElement = <textarea className={inputStyles.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
+            break;
+        case ( 'select' ):
+            inputElement = (
+                <select
+                    className={inputStyles.join(' ')}
+                    onChange={props.changed}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (<option key={option.value} value={option.value}>{option.displayValue}</option>))}
+                </select>);
             break;
         default:
-            inputElement = <input className={styles.InputElement} {...props} />;
+            inputElement = <input className={inputStyles.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
     }
 
     return (
