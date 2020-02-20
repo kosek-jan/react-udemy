67dbf0109012d94f581a671444c68a991c852da8
Kosek Jan <Jan.Kosek@tescosw.cz>
2020-02-20 07:32:48 +0100

    přidání reduxu - burger builder

--- react-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,12 +27,12 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://burgerbuilder-8f3da.firebaseio.com/ingredients.json')
-            .then(res => {
-                this.setState({ingredients: res.data});
-            }).catch(error => {
-                this.setState({error: true});
-            });
+        // axios.get('https://burgerbuilder-8f3da.firebaseio.com/ingredients.json')
+        //     .then(res => {
+        //         this.setState({ingredients: res.data});
+        //     }).catch(error => {
+        //         this.setState({error: true});
+        //     });
     }
 
     updatePurchaseState = (ingredients) => {
